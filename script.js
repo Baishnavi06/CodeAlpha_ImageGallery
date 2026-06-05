6e0587d1a3f37c79a4149f0ff8a302dfa958e26b
Baishnavo06 <baishnavi0606@gmailcom>
2026-06-05 17:03:27 +0530

    Implemented dark mode teheme and masonry styling

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const lightbox = document.getElementById('lightbox');
 const lightboxImg = document.getElementById('lightbox-img');
+const downloadBtn = document.getElementById('download-btn');
 const allImages = document.querySelectorAll('.gallery-item');
 
 allImages.forEach((img) => {
     img.addEventListener('click', (e) => {
         lightbox.style.display = 'flex';
         lightboxImg.src = e.target.src;
+        downloadBtn.href = e.target.src;
         document.body.style.overflow = 'hidden';
     });
 });
@@ -18,5 +20,26 @@ function closeLightbox() {
 lightbox.addEventListener('click', (e) => {
     if (e.target === lightbox) {
         closeLightbox();
+    }
+});
+
+const themeToggleBtn = document.getElementById('theme-toggle');
+const currentTheme = localStorage.getItem('theme');
+
+if(currentTheme === 'dark') {
+    document.body.setAttribute('data-theme', 'dark');
+    themeToggleBtn.textContent = 'Toggle Light Mode 🌞';
+}
+
+themeToggleBtn.addEventListener('click', () => {
+    let theme = document.body.getAttribute('data-theme');
+    if (theme === 'dark') {
+        document.body.removeAttribute('data-theme');
+        localStorage.setItem('theme', 'light');
+        themeToggleBtn.textContent = 'Toggle Dark Mode 🌙';
+    } else {
+        document.body.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+        themeToggleBtn.textContent = 'Toggle Light Mode 🌞';
     }
 });
